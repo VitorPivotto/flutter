a93dbc881e5e90d31251ba20c1b9fac207ff0c60
VitorPivotto <vitor.pivotto@etec.sp.gov.br>
2020-02-27 14:30:28 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart' ;
+void main() => runApp(
+  MaterialApp(
+    home: Center(
+      child: Container(
+        color: Colors.blue[100],
+        child: Text("Vitor Pivotto",
+            style: TextStyle(color: Colors.black)
+        ),
+      ),
+    ),
+  ),
+);
